817a8c586ff4d0091175dd17b899204a5929a8dc
yangtuwatuwa <rakapanggalih@gmail.com>
2025-02-20 14:10:48 +0700

    tugassshhh

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ let absensi;
 function mulaiiii(){
     do {
         let bonus = 0;
-        
         if (i > 20) {
             bonus = 500000;
         }
         absensi = confirm("Apakah anda telat?");
-        
         let hasil = gaji + (200000 * i) + bonus; 
-        alert(`Gaji hari ke-${i} berjumlah: ${hasil}`);
-    
-        
+        alert(`Gaji hari ke-${i} berjumlah: ${hasil}`)
         if (absensi == true) {
             hasil -= 5000000;
             alert(`Yahh, Anda telat! KAMU SAYA PECAT DAN Gaji dipotong 5jt. Sisa gaji: ${hasil}`);
@@ -21,5 +17,4 @@ function mulaiiii(){
         }
         i++; 
     } while (absensi === false); 
-
 }
